c5817dc86f8faad604c3d02857563276986fda96
Timur Meyster <timur.meyster@gmail.com>
2015-06-14 15:22:42 -0700

    added initialize method, validate input and respective tests for these methods

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,51 @@
 
 class Cuboid
+  attr_reader :origin
   
+  def initialize(origin, h: 0, w:0, l:0)
+    @origin = origin #[h, w, l]
+    @h = h
+    @w = w
+    @l = l
+
+    validate_input!
+    # generate_vertices
+  end
   #BEGIN public methods that should be your starting point
 
   def move_to!(x, y, z)
+
   end
-  
+
+
   def vertices
   end
-  
+
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
   end
 
-  #END public methods that should be your starting point  
+  private
+
+  def generate_vertices(new_x, new_y, new_z)
+
+  end
+
+  def validate_input!
+    raise ArgumentError.new("Origin must be present") unless (!@origin.nil? && @origin.length == 3)
+
+    [@h, @w, @l].each do |side|
+      if (side <= 0 || !side.is_a?(Integer))
+        raise ArgumentError.new("Please enter positive integers for length/width/height")
+      end
+    end
+  end
+
+
+
+  #END public methods that should be your starting point
 end
+
+#
+# cuboid1 = Cuboid.new([0, 0, 0], h:4, w:5, l:1 )
+# p cuboid1
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,44 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+
+  # describe "move_to" do
+  #   it "changes the origin in the simple happy case" do
+  #     expect(subject.move_to!(1,2,3)).to be true
+  #   end
+  # end
+  #
+  # describe "intersects?" do
+  # end
+
+  describe "initialize a cuboid" do
+
+    it "initilizes a cuboid correctly when all params are valid" do
+      cuboid1 = Cuboid.new([0, 0, 0], h:3, w:4, l:5 )
+      expect(cuboid1).to be_a Cuboid
     end
-  end    
-  
-  describe "intersects?" do
+
+    it "raises an error if paremters passed in are invalid dimentions" do
+
+      expect{Cuboid.new([0, 0, 0], h:0, w:4, l:5 )}.to raise_error(ArgumentError)
+      expect{Cuboid.new([0, 0, 0], h:3, w:0, l:5 )}.to raise_error(ArgumentError)
+      expect{Cuboid.new([0, 0, 0], h:3, w:4, l:0 )}.to raise_error(ArgumentError)
+      expect{Cuboid.new([0, 0], h:3, w:4, l:5 )}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "move_to!" do
+
+    let(:subject) { Cuboid.new([0, 0, 0], h:3, w:4, l:5 ) }
+
+    it "should move the origin to a new origin" do
+      subject.move_to!(3,2,1)
+      expect(subject.origin).to eq([3, 2, 1])
+    end
+
   end
 
 end
